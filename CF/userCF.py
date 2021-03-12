5803f784ab267ad0b232afc4b1b982cddc4ba0c1
zone <1045786544@qq.com>
2021-03-12 14:14:16 +0800

    2021年3月12日14点14分

--- CF/userCF.py
+++ CF/userCF.py
@@ -1,175 +1,139 @@
-import sys
 import random
+
 import math
-import os
 from operator import itemgetter
 
-random.seed(0)
 
-
-class UserBasedCF(object):
-    ''' TopN recommendation - User Based Collaborative Filtering '''
-
+class UserBasedCF():
+    # 初始化相关参数
     def __init__(self):
-        self.trainset = {}
-        self.testset = {}
-
+        # 找到与目标用户兴趣相似的20个用户，为其推荐10部电影
         self.n_sim_user = 20
         self.n_rec_movie = 10
 
-        self.user_sim_mat = {}
-        self.movie_popular = {}
+        # 将数据集划分为训练集和测试集
+        self.trainSet = {}
+        self.testSet = {}
+
+        # 用户相似度矩阵
+        self.user_sim_matrix = {}
         self.movie_count = 0
 
-        print('Similar user number = %d' % self.n_sim_user, file=sys.stderr)
-        print('recommended movie number = %d' %
-              self.n_rec_movie, file=sys.stderr)
+        print('Similar user number = %d' % self.n_sim_user)
+        print('Recommneded movie number = %d' % self.n_rec_movie)
 
-    @staticmethod
-    def loadfile(filename):
-        ''' load a file, return a generator. '''
-        fp = open(filename, 'r')
-        for i, line in enumerate(fp):
-            yield line.strip('\r\n')
-            if i % 100000 == 0:
-                print('loading %s(%s)' % (filename, i), file=sys.stderr)
-        fp.close()
-        print('load %s succ' % filename, file=sys.stderr)
 
-    def generate_dataset(self, filename, pivot=0.7):
-        ''' load rating data and split it to training set and test set '''
-        trainset_len = 0
-        testset_len = 0
-
-        for line in self.loadfile(filename):
-            user, movie, rating, _ = line.split('::')
-            # split the data by pivot
+    # 读文件得到“用户-电影”数据
+    def get_dataset(self, filename, pivot=0.75):
+        trainSet_len = 0
+        testSet_len = 0
+        for line in self.load_file(filename):
+            user, movie, rating, timestamp = line.split('::')
             if random.random() < pivot:
-                self.trainset.setdefault(user, {})
-                self.trainset[user][movie] = int(rating)
-                trainset_len += 1
+                self.trainSet.setdefault(user, {})
+                self.trainSet[user][movie] = rating
+                trainSet_len += 1
             else:
-                self.testset.setdefault(user, {})
-                self.testset[user][movie] = int(rating)
-                testset_len += 1
+                self.testSet.setdefault(user, {})
+                self.testSet[user][movie] = rating
+                testSet_len += 1
+        print('Split trainingSet and testSet success!')
+        print('TrainSet = %s' % trainSet_len)
+        print('TestSet = %s' % testSet_len)
 
-        print('split training set and test set succ', file=sys.stderr)
-        print('train set = %s' % trainset_len, file=sys.stderr)
-        print('test set = %s' % testset_len, file=sys.stderr)
 
+    # 读文件，返回文件的每一行
+    def load_file(self, filename):
+        with open(filename, 'r') as f:
+            for i, line in enumerate(f):
+                if i == 0:  # 去掉文件第一行的title
+                    continue
+                yield line.strip('\r\n')
+        print('Load %s success!' % filename)
+
+
+    # 计算用户之间的相似度
     def calc_user_sim(self):
-        ''' calculate user similarity matrix '''
-        # build inverse table for item-users
-        # key=movieID, value=list of userIDs who have seen this movie
-        print('building movie-users inverse table...', file=sys.stderr)
-        movie2users = dict()
-
-        for user, movies in self.trainset.items():
+        # 构建“电影-用户”倒排索引
+        # key = movieID, value = list of userIDs who have seen this movie
+        print('Building movie-user table ...')
+        movie_user = {}
+        for user, movies in self.trainSet.items():
             for movie in movies:
-                # inverse table for item-users
-                if movie not in movie2users:
-                    movie2users[movie] = set()
-                movie2users[movie].add(user)
-                # count item popularity at the same time
-                if movie not in self.movie_popular:
-                    self.movie_popular[movie] = 0
-                self.movie_popular[movie] += 1
-        print('build movie-users inverse table succ', file=sys.stderr)
+                if movie not in movie_user:
+                    movie_user[movie] = set()
+                movie_user[movie].add(user)
+        print('Build movie-user table success!')
 
-        # save the total movie number, which will be used in evaluation
-        self.movie_count = len(movie2users)
-        print('total movie number = %d' % self.movie_count, file=sys.stderr)
+        self.movie_count = len(movie_user)
+        print('Total movie number = %d' % self.movie_count)
 
-        # count co-rated items between users
-        usersim_mat = self.user_sim_mat
-        print('building user co-rated movies matrix...', file=sys.stderr)
-
-        for movie, users in movie2users.items():
+        print('Build user co-rated movies matrix ...')
+        for movie, users in movie_user.items():
             for u in users:
                 for v in users:
                     if u == v:
                         continue
-                    usersim_mat.setdefault(u, {})
-                    usersim_mat[u].setdefault(v, 0)
-                    usersim_mat[u][v] += 1
-        print('build user co-rated movies matrix succ', file=sys.stderr)
+                    self.user_sim_matrix.setdefault(u, {})
+                    self.user_sim_matrix[u].setdefault(v, 0)
+                    self.user_sim_matrix[u][v] += 1
+        print('Build user co-rated movies matrix success!')
 
-        # calculate similarity matrix
-        print('calculating user similarity matrix...', file=sys.stderr)
-        simfactor_count = 0
-        PRINT_STEP = 2000000
-
-        for u, related_users in usersim_mat.items():
+        # 计算相似性
+        print('Calculating user similarity matrix ...')
+        for u, related_users in self.user_sim_matrix.items():
             for v, count in related_users.items():
-                usersim_mat[u][v] = count / math.sqrt(
-                    len(self.trainset[u]) * len(self.trainset[v]))
-                simfactor_count += 1
-                if simfactor_count % PRINT_STEP == 0:
-                    print('calculating user similarity factor(%d)' %
-                          simfactor_count, file=sys.stderr)
+                self.user_sim_matrix[u][v] = count / math.sqrt(len(self.trainSet[u]) * len(self.trainSet[v]))
+        print('Calculate user similarity matrix success!')
 
-        print('calculate user similarity matrix(similarity factor) succ',
-              file=sys.stderr)
-        print('Total similarity factor number = %d' %
-              simfactor_count, file=sys.stderr)
 
+    # 针对目标用户U，找到其最相似的K个用户，产生N个推荐
     def recommend(self, user):
-        ''' Find K similar users and recommend N movies. '''
         K = self.n_sim_user
         N = self.n_rec_movie
-        rank = dict()
-        watched_movies = self.trainset[user]
+        rank = {}
+        watched_movies = self.trainSet[user]
 
-        for similar_user, similarity_factor in sorted(self.user_sim_mat[user].items(),
-                                                      key=itemgetter(1), reverse=True)[0:K]:
-            for movie in self.trainset[similar_user]:
+        # v=similar user, wuv=similar factor
+        for v, wuv in sorted(self.user_sim_matrix[user].items(), key=itemgetter(1), reverse=True)[0:K]:
+            for movie in self.trainSet[v]:
                 if movie in watched_movies:
                     continue
-                # predict the user's "interest" for each movie
                 rank.setdefault(movie, 0)
-                rank[movie] += similarity_factor
-        # return the N best movies
+                rank[movie] += wuv
         return sorted(rank.items(), key=itemgetter(1), reverse=True)[0:N]
 
-    def evaluate(self):
-        ''' print evaluation result: precision, recall, coverage and popularity '''
-        print('Evaluation start...', file=sys.stderr)
 
+    # 产生推荐并通过准确率、召回率和覆盖率进行评估
+    def evaluate(self):
+        print("Evaluation start ...")
         N = self.n_rec_movie
-        #  varables for precision and recall
+        # 准确率和召回率
         hit = 0
         rec_count = 0
         test_count = 0
-        # varables for coverage
+        # 覆盖率
         all_rec_movies = set()
-        # varables for popularity
-        popular_sum = 0
 
-        for i, user in enumerate(self.trainset):
-            if i % 500 == 0:
-                print('recommended for %d users' % i, file=sys.stderr)
-            test_movies = self.testset.get(user, {})
+        for i, user, in enumerate(self.trainSet):
+            test_movies = self.testSet.get(user, {})
             rec_movies = self.recommend(user)
-            for movie, _ in rec_movies:
+            for movie, w in rec_movies:
                 if movie in test_movies:
                     hit += 1
                 all_rec_movies.add(movie)
-                popular_sum += math.log(1 + self.movie_popular[movie])
             rec_count += N
             test_count += len(test_movies)
 
         precision = hit / (1.0 * rec_count)
         recall = hit / (1.0 * test_count)
         coverage = len(all_rec_movies) / (1.0 * self.movie_count)
-        popularity = popular_sum / (1.0 * rec_count)
-
-        print('precision=%.4f\trecall=%.4f\tcoverage=%.4f\tpopularity=%.4f' %
-              (precision, recall, coverage, popularity), file=sys.stderr)
+        print('precisioin=%.4f\trecall=%.4f\tcoverage=%.4f' % (precision, recall, coverage))
 
 
 if __name__ == '__main__':
-    ratingfile = os.path.join('../data/ml-1m', 'ratings.dat')
-    usercf = UserBasedCF()
-    usercf.generate_dataset(ratingfile)
-    usercf.calc_user_sim()
-    usercf.evaluate()
+    rating_file = '../data/ml-1m/ratings.dat'
+    userCF = UserBasedCF()
+    userCF.get_dataset(rating_file)
+    userCF.calc_user_sim()
+    userCF.evaluate()
