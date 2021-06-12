5fd99ffd62d1f13ea909ea4afef55d9062186dd4
zone <1045786544@qq.com>
2021-06-12 20:51:38 +0800

    2021年6月12日20点51分

--- MyModel/test/evaluate.py
+++ MyModel/test/evaluate.py
@@ -33,54 +33,38 @@ print('test rmse: %f', np.sqrt(model.evaluate(test_X, test_y)[2]))
         4) 对每个用户的TOP-N推荐的召回率进行整合，形成一个总指标。
 '''
 
-item_num = dataset.get_feature()[1]['feat_num']
-all_items = set(range(1, item_num + 1))
+
+def get_recommend(userId, list):
+    temp_list = [[int(userId), int(m_id)] for m_id in list]
+    pred_rating = model.predict(temp_list, batch_size=500).flatten()
+    return [item[0] for item in sorted(zip(list, pred_rating), key=lambda x: x[1], reverse=True)]
 
 
-def get_recommend(id, list, N):
-    temp_list = np.empty(shape=(len(list), 2), dtype='int32')
-    temp_list[:, 0] = id
-    temp_list[:, 1] = list
-    temp_df = pd.DataFrame(temp_list, columns=['UserId', 'MovieId'])
-    temp_df['Rating'] = model.predict(temp_df.values, batch_size=500)
-    return temp_df.set_index('MovieId').sort_values(by='Rating', ascending=False).index[:N]
+def rec(train, test, N):
+    item_num = dataset.get_feature()[1]['feat_num']
+    all_items = set(range(1, item_num + 1))
+    hits = {}
+    for n in N:
+        hits.setdefault(n, 0)
+    test_count = 0
 
-
-def eva_rec(train, test, N):
-    hit_rec = 0
-    all_rec = 0
     # 统计结果
     # user_num = dataset.get_feature()[0]['feat_num']
-    for user_id in tqdm(test.index.unique().values):
-        train_items = set(train.loc[user_id].values.flatten())
-        test_items = set(test.loc[user_id].values.flatten())
+    for user_id in tqdm(test):
+        train_items = train[user_id]
+        test_items = test[user_id]
         other_items = all_items - train_items.union(test_items)
         for idx in test_items:
             random_items = random.sample(other_items, 500)
             random_items.append(idx)
             # =================================获取排序后二级索引中的电影号=========================================
-            sort_values = get_recommend(user_id, random_items, N)
-            hit_rec += int(idx in set(sort_values))
-
-        all_rec += len(test_items)
-    return hit_rec / (all_rec * 1.0)
-
-
-def eva_acc(train, test, N):
-    hit_acc = 0
-    all_acc = 0
-    # 统计结果
-    for user_id in tqdm(test.index.unique().values):
-        train_items = set(train.loc[user_id].values.flatten())
-        test_items = set(test.loc[user_id].values.flatten())
-        other_items = all_items - train_items.union(test_items)
-        random_items = random.sample(other_items, 100)
-        random_items += list(test_items)
-        # =================================获取排序后二级索引中的电影号=========================================
-        sort_values = get_recommend(user_id, random_items, N)
-        hit_acc += len(set(sort_values) & test_items)
-        all_acc += N
-    return hit_acc / (all_acc * 1.0)
+            sort_values = get_recommend(user_id, random_items)
+            for n in N:
+                hits[n] += int(idx in sort_values[:n])
+        test_count += len(test_items)
+    for n in N:
+        recall = hits[n] / (1.0 * test_count)
+        print('N:%d\trecall=%.6f\t' % (n, recall))
 
 
 # =========================main===============================
